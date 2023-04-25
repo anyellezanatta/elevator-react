e56f7adb788e466e72aa8f4f5d59015269e7325f
Anyelle Zanatta <anyelle.ad@gmail.com>
2023-04-25 13:49:16 +0200

    Jest config, some tests

---
+++ src/lib/utils.spec.ts
@@ -0,0 +1,39 @@
+import { rangeDesc, removeBy } from "./utils";
+
+describe("utils", () => {
+  describe("rangeDesc", () => {
+    const cases = [
+      { size: 0, start: 0, expected: [] },
+      { size: 5, start: 4, expected: [4, 3, 2, 1, 0] },
+      { size: 3, start: 3, expected: [3, 2, 1] },
+    ];
+
+    cases.forEach(({ size, start, expected }) => {
+      it(`should generate the range ${expected} for the size ${size} starting at ${start}`, () => {
+        // AAA
+        const actual = rangeDesc(size, start);
+
+        expect(actual).toStrictEqual(expected);
+      });
+    });
+  });
+
+  describe("removeBy", () => {
+    it("should remove the item when it's found", () => {
+      const expected = [{ size: 10 }, { size: 30 }];
+      const items = [{ size: 10 }, { size: 20 }, { size: 30 }];
+
+      const actual = removeBy(items, "size", 20);
+
+      expect(actual).toStrictEqual(expected);
+    });
+
+    it("should not remove when the item is not found", () => {
+      const items = [{ name: "Anna" }, { name: "Any" }];
+
+      const actual = removeBy(items, "name", "Peter");
+
+      expect(actual).toStrictEqual(items);
+    });
+  });
+});
